e8e8e52b70462d3ed33cbc495cc206bf7e2aeab7
Arne <a.oetersen@stud.fh-luebeck.de>
2018-05-03 16:47:49 +0200

    .

--- src/main/java/shop/resources/KundenResource.java
+++ src/main/java/shop/resources/KundenResource.java
@@ -1,4 +1,12 @@
 package shop.resources;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 public class KundenResource {
+
+
 }
