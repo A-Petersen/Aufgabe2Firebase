60966b7de7f30c50b4ec8a1bb435a0a4a31ca1cb
Arne <a.oetersen@stud.fh-luebeck.de>
2018-05-03 15:07:30 +0200

    .

---
+++ src/main/java/shop/model/Kunde.java
@@ -0,0 +1,17 @@
+package shop.model;
+
+public class Kunde {
+
+    public String kundenNr;
+
+    public String vorname;
+
+    public String nachname;
+
+    public String geschlecht;
+
+    public String gebDatum;
+
+    public String bankverbindung_Iban;
+
+}
