1335ff187a9ce35caf23acdb9a46f59a0b516c1b
Arne <a.oetersen@stud.fh-luebeck.de>
2018-05-03 15:05:32 +0200

    Initial commit

---
+++ src/main/java/shop/model/Product.java
@@ -0,0 +1,16 @@
+package shop.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement // required for XML binding
+public class Product {
+
+	public String id;
+
+	public String title;
+
+	public String description;
+
+	public Integer currentPrice;
+
+}
