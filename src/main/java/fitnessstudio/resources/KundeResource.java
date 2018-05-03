dcc6094f8dbd3b256539d5268be96d661c06ae90
Arne <a.oetersen@stud.fh-luebeck.de>
2018-05-03 17:49:37 +0200

    .

--- src/main/java/fitnessstudio/resources/KundeResource.java
+++ src/main/java/fitnessstudio/resources/KundeResource.java
@@ -3,10 +3,11 @@ package fitnessstudio.resources;
 import fitnessstudio.FitnessstudioService;
 import fitnessstudio.model.Kunde;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 public class KundeResource {
 
@@ -17,6 +18,7 @@ public class KundeResource {
     }
 
     @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getKunde() {
         Kunde kunde = FitnessstudioService.kunden.get(kundenNr);
         if (kunde == null) {
@@ -26,7 +28,8 @@ public class KundeResource {
     }
 
     @PUT
-    public Response putKunde(Kunde kunde) {
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response putKunde(Kunde kunde, @Context UriInfo uriInfo) {
         boolean exists = FitnessstudioService.kunden.get(kundenNr) != null;
         if (!exists) {
             return Response.status(404).build();
--- src/main/java/fitnessstudio/resources/KundenResource.java
+++ src/main/java/fitnessstudio/resources/KundenResource.java
@@ -1,12 +1,46 @@
 package fitnessstudio.resources;
 
+import com.google.firebase.database.DatabaseReference;
+import fitnessstudio.FitnessstudioService;
+import fitnessstudio.ShopService;
+import fitnessstudio.model.Kunde;
+import fitnessstudio.model.Product;
 import jdk.nashorn.internal.objects.annotations.Getter;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.Collection;
 
+@Path("/kunden")
 public class KundenResource {
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Collection<Kunde> getKunden() {
+        return FitnessstudioService.kunden.values();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response postKunden(Kunde kunde, @Context UriInfo uriInfo) {
+
+        DatabaseReference ref = FitnessstudioService.kundenRef.push();
+        kunde.kundenNr = ref.getKey();
+        ref.setValueAsync(kunde);
+
+        URI uri = uriInfo.getAbsolutePathBuilder().path(kunde.kundenNr).build();
+
+        return Response.created(uri).entity(kunde).build(); // 201
+    }
+
+    @Path("{kundenNr}")
+    public KundeResource getKunden(@PathParam("kundenNr") String kundenNr) {
+        return new KundeResource(kundenNr);
+    }
 
 
 }
